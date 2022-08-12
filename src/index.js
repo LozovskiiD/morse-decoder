51305346a166383bc98b85f2cf018962bd8ef01d
LozovskiiD <lozdm@yandex.ru>
2022-08-12 19:39:14 +0300

    test passed

--- src/index.js
+++ src/index.js
@@ -37,8 +37,27 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+const arrKeys=Object.keys(MORSE_TABLE)
+function decode(expr) {    
+    let phrase = ''
+    for (let i=0; i< expr.length/10 ; i++) {
+        let newLetter = expr.substr(i*10,10)
+        newLetter=newLetter.substr(newLetter.indexOf('1'))
+       newLetter = newLetter.replace(/10/gi,'.')      
+       newLetter = newLetter.replace(/11/gi,'-') 
+      if (newLetter == '*') {
+         phrase=phrase+' '
+      }
+      else {
+       arrKeys.forEach((item) => {
+         if (item == newLetter) {
+           phrase = phrase+MORSE_TABLE[item]
+         }        
+       })  
+    }
+    }  
+  
+   return phrase
 }
 
 module.exports = {
